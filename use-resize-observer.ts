b1140eaae81e34ae01fb5d48149c76b678a155a0
Ole Kristian Fjærestad <fjaerestad.olekristian@gmail.com>
2022-03-07 13:51:40 +0100

    Create use-resize-observer.ts

---
+++ use-resize-observer.ts
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+/**
+ * Get width and height of an element, accounting for resize events.
+ */
+export function useResizeObserver(
+  el: Element,
+  options?: ResizeObserverOptions
+): [width: number, height: number] {
+  const [width, setWidth] = useState<number>(0);
+  const [height, setHeight] = useState<number>(0);
+
+  const observer = new ResizeObserver((entries) => {
+    const entry = entries[0];
+
+    // Firefox implements `contentBoxSize` as a single content rect, rather than an array.
+    const contentBoxSize: ResizeObserverSize = Array.isArray(
+      entry.contentBoxSize
+    )
+      ? entry.contentBoxSize[0]
+      : entry.contentBoxSize;
+
+    // Fallback for older browsers.
+    const [w, h] = contentBoxSize
+      ? [contentBoxSize.inlineSize, contentBoxSize.blockSize]
+      : [entry.contentRect.width, entry.contentRect.height];
+
+    setWidth(w);
+    setHeight(h);
+  });
+
+  observer.observe(el, options);
+
+  return [width, height];
+}
