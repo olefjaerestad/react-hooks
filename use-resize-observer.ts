aaf2cd6024f256e60d7b7f4551b31736bd2fd9bb
Ole Kristian Fjærestad <fjaerestad.olekristian@gmail.com>
2022-04-10 11:32:23 +0200

    useResizeObserver: cleanup and optimize.
    * Add disconnect observer cleanup function.
    * Memoize observer.

--- use-resize-observer.ts
+++ use-resize-observer.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 /**
  * Get width and height of an element, accounting for resize events.
@@ -10,27 +10,33 @@ export function useResizeObserver(
   const [width, setWidth] = useState<number>(0);
   const [height, setHeight] = useState<number>(0);
 
-  const observer = new ResizeObserver((entries) => {
-    const entry = entries[0];
+  const observer = useMemo(() => {
+    return new ResizeObserver((entries) => {
+      const entry = entries[0];
 
-    // Firefox implements `contentBoxSize` as a single content rect, rather than an array.
-    const contentBoxSize: ResizeObserverSize = Array.isArray(
-      entry.contentBoxSize
-    )
-      ? entry.contentBoxSize[0]
-      : entry.contentBoxSize;
+      // Firefox implements `contentBoxSize` as a single content rect, rather than an array.
+      const contentBoxSize: ResizeObserverSize = Array.isArray(
+        entry.contentBoxSize
+      )
+        ? entry.contentBoxSize[0]
+        : entry.contentBoxSize;
 
-    // Fallback for older browsers.
-    const [w, h] = contentBoxSize
-      ? [contentBoxSize.inlineSize, contentBoxSize.blockSize]
-      : [entry.contentRect.width, entry.contentRect.height];
+      // Fallback for older browsers.
+      const [w, h] = contentBoxSize
+        ? [contentBoxSize.inlineSize, contentBoxSize.blockSize]
+        : [entry.contentRect.width, entry.contentRect.height];
 
-    setWidth(w);
-    setHeight(h);
-  });
+      setWidth(w);
+      setHeight(h);
+    });
+  }, []);
 
   useEffect(() => {
     el && observer.observe(el, options);
+
+    return () => {
+      el && observer.disconnect();
+    };
   }, [el]);
 
   return [width, height];
