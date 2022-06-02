c8ac0e35d178f1e9810f24ce0d4895dca4385dcd
Ole Kristian Fjærestad <fjaerestad.olekristian@gmail.com>
2022-06-02 09:44:30 +0200

    Add react-router-dom v6 support to useHasNavigated

--- use-has-navigated.ts
+++ use-has-navigated.ts
@@ -1,3 +1,7 @@
+/**
+ * The first version is for react-router-dom v5, the second one is for react-router-dom v6.
+ */
+
 import { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
@@ -9,7 +13,7 @@ let isListening: boolean = false;
  * has happened or not during the lifetime of the application.
  * Pass the `resetOnComponentUnmount` argument to reset navigation status
  * when component unmounts.
- * Requires react-router-dom.
+ * Requires react-router-dom < 6.
  */
 export function useHasNavigated(resetOnComponentUnmount: boolean = false) {
   const history = useHistory();
@@ -38,6 +42,42 @@ export function useHasNavigated(resetOnComponentUnmount: boolean = false) {
       }
     };
   }, [isListening, resetOnComponentUnmount]);
+
+  return hasNavigated;
+}
+
+/** ------------------------------------------------- */
+
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+let hasNavigated = false;
+
+/**
+ * Returns true or false depending on whether a navigation (i.e. route change)
+ * has happened or not during the lifetime of the application.
+ * Pass the `resetOnComponentUnmount` argument to reset navigation status
+ * when component unmounts.
+ * Requires react-router-dom >= 6.
+ */
+export function useHasNavigated(resetOnComponentUnmount = false) {
+  const { pathname } = useLocation();
+  const [prevPathname, setPrevPathname] = useState(pathname);
+
+  useEffect(() => {
+    if (pathname !== prevPathname) {
+      setPrevPathname(pathname);
+      hasNavigated = true;
+    }
+  }, [pathname, prevPathname]);
+
+  useEffect(() => {
+    return () => {
+      if (resetOnComponentUnmount) {
+        hasNavigated = false;
+      }
+    };
+  }, [resetOnComponentUnmount]);
 
   return hasNavigated;
 }
