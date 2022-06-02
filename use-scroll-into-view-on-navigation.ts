ff24077f2a8c2f0ea755833efce635ced42c6c0f
Ole Kristian Fjærestad <fjaerestad.olekristian@gmail.com>
2022-06-02 09:47:15 +0200

    Add react-router-dom v6 support to useScrollIntoViewOnNavigation

--- use-scroll-into-view-on-navigation.ts
+++ use-scroll-into-view-on-navigation.ts
@@ -1,9 +1,13 @@
+/**
+ * The first version is for react-router-dom v5, the second one is for react-router-dom v6.
+ */
+
 import { MutableRefObject, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 /**
  * Scroll a given element into view on navigation (i.e. route change).
- * Requires react-router-dom.
+ * Requires react-router-dom < 6.
  */
 export function useScrollIntoViewOnNavigation(
   ref: MutableRefObject<HTMLElement | undefined>
@@ -19,4 +23,27 @@ export function useScrollIntoViewOnNavigation(
       unlisten();
     };
   }, []);
+}
+
+/** ----------------------------------------------------- */
+
+import { MutableRefObject, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom-6';
+
+/**
+ * Scroll a given element into view on navigation (i.e. route change).
+ * Requires react-router-dom >= 6.
+ */
+export function useScrollIntoViewOnNavigation(
+  ref: MutableRefObject<HTMLElement | undefined>
+) {
+  const { pathname } = useLocation();
+  const [prevPathname, setPrevPathname] = useState(pathname);
+
+  useEffect(() => {
+    if (pathname !== prevPathname) {
+      setPrevPathname(pathname);
+      ref.current?.scrollIntoView();
+    }
+  }, [pathname, prevPathname]);
 }
