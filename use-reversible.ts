abefec2697fa3256644472a26fac6b172e880ff8
Ole Kristian Fjærestad <fjaerestad.olekristian@gmail.com>
2023-08-17 17:02:37 +0200

    Create use-reversible.ts

---
+++ use-reversible.ts
@@ -0,0 +1,64 @@
+import { useCallback, useRef, useState } from 'react';
+
+type IReversibleDispatch<T extends unknown> = (value: IReversibleSetStateAction<T>) => IUndoFunction;
+type IReversibleSetStateAction<T extends unknown> = T | ((current: T, previous: T) => T);
+type IUndoFunction = () => void;
+
+/**
+ * A hook to achieve reversible state changes. Useful for example with
+ * optimistic updates and rollback on error.
+ *
+ * Works like `useState`, with the exception that the callback function in
+ * `setState` now takes a second parameter `previousState`. This enables
+ * easy rollback of the previous state update. If you don't want to manage the
+ * rollback yourself, `setState` also returns a parameterless `undo` function
+ * that undoes the state change.
+ *
+ * @example
+ * const [name, setName] = useState('John');
+ *
+ * async function updateName(newName: string) {
+ *   const undo = setName(newName);
+ *   try {
+ *     await saveName(newName);
+ *   } catch {
+ *     setName((current, previous) => previous);
+ *     // OR
+ *     undo();
+ *   }
+ * }
+ *
+ * <p>{name}</p>
+ */
+export function useReversible(initialValue?: undefined): [undefined, IReversibleDispatch<undefined>];
+export function useReversible<T extends unknown>(initialValue: T): [T, IReversibleDispatch<T>];
+export function useReversible<T extends unknown>(initialValue?: T): [T | undefined, IReversibleDispatch<T | undefined>];
+export function useReversible<T extends unknown>(initialValue: T): [T, IReversibleDispatch<T>] {
+  const prevValue = useRef<T>(initialValue);
+  const [value, setValue] = useState(initialValue);
+
+  const setValueReversible: IReversibleDispatch<T> = useCallback((val) => {
+    let c: T;
+    let p: T = prevValue.current;
+
+    setValue((current) => {
+      // @ts-ignore: Type 'T & Function' has no call signatures.
+      const newValue = typeof val === 'function' ? val(current, prevValue.current) : val;
+      c = newValue;
+      if (newValue !== current) {
+        prevValue.current = current;
+        p = current;
+      }
+      return newValue;
+    });
+
+    return function undo() {
+      setValue(() => {
+        prevValue.current = p;
+        return p;
+      });
+    };
+  }, []);
+
+  return [value, setValueReversible];
+}
