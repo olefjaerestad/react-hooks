7d91a8dccd3a62928bf09246afac4cf8ce141b84
Ole Kristian Fjærestad <fjaerestad.olekristian@gmail.com>
2023-08-30 16:26:12 +0200

    Update use-state-if-mounted.ts

--- use-state-if-mounted.ts
+++ use-state-if-mounted.ts
@@ -11,6 +11,8 @@ import {
  * A drop-in replacement for `useState` that prevents state updates on unmounted components.
  * Useful if you're setting state after waiting for a promise, and there's a chance
  * the component has unmounted by the time the promise is settled.
+ * 
+ * Note: This hook is no longer relevant in React >= 18. See https://github.com/facebook/react/pull/22114
  */
 export function useStateIfMounted<T = undefined>(): [
   state: T | undefined,
