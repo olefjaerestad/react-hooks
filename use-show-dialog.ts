5699c8de72771ae6d85aa70d6940217985bc2c9a
Ole Kristian Fjærestad <fjaerestad.olekristian@gmail.com>
2025-05-27 09:46:12 +0200

    Update use-show-dialog.ts

--- use-show-dialog.ts
+++ use-show-dialog.ts
@@ -1,4 +1,4 @@
-import { MutableRefObject, useEffect } from 'react';
+import { MutableRefObject, useEffect, useRef } from 'react';
 
 /**
  * Show or hide a dialog element.
@@ -18,6 +18,8 @@ function useShowDialog({
   mode: 'modal' | 'nonmodal';
   open: boolean;
 }) {
+  const currentMode = useRef(mode);
+  
   useEffect(() => {
     if (disabled) return;
 
@@ -29,10 +31,34 @@ function useShowDialog({
       if (open) {
         if (!dialog.current?.open) {
           mode === 'modal' ? dialog.current?.showModal() : dialog.current?.show();
+          currentMode.current = mode;
+        } else if (mode !== currentMode.current) {
+          // Close and reopen on mode change. The reopening needs a timeout of a
+          // bit longer than the dialog's animation/transition duration to ensure
+          // the mode change works correctly.
+
+          // Prevents flash of incorrect position.
+          dialog.current.style.animationDuration = '0s';
+          dialog.current.style.transitionDuration = '0s';
+
+          dialog.current?.close();
+
+          setTimeout(() => {
+            if (dialog.current) {
+              dialog.current.style.animationDuration = '';
+              dialog.current.style.transitionDuration = '';
+            }
+
+            mode === 'modal'
+              ? dialog.current?.showModal()
+              : dialog.current?.show();
+            currentMode.current = mode;
+          }, 10);
         }
       } else {
         if (dialog.current?.open) {
           dialog.current?.close();
+          currentMode.current = mode;
         }
       }
     }, 5); // 0 works for all browsers except Safari (tested in 17.4.1).
